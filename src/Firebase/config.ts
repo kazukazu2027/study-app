fa7a0b80d14f345c83604445478384160d3f7eb1
kazukazu2027 <kazu2000027@gmail.com>
2021-09-07 20:42:14 +0900

    エラー修正

--- src/Firebase/config.ts
+++ src/Firebase/config.ts
@@ -1,7 +1,8 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  databaseURL:
+    "https://study-service-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
