799d5cf1b278b562913a73a18d36a1bdf2e5419f
kazukazu2027 <kazu2000027@gmail.com>
2021-09-07 20:34:35 +0900

    region変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -30,3 +30,10 @@ export const addQuestionListSet = functions.https.onRequest(
     }
   }
 );
+
+export const testDelete = functions
+  .region("asia-northeast1")
+  .firestore.document("test/{testId}")
+  .onDelete(async (snap, context) => {
+    // hogehoge
+  });
