d6b8c74b2938da5b7ffb8d9dfde7bae9c0aa3a94
kazukazu2027 <kazu2000027@gmail.com>
2021-09-09 19:14:31 +0900

    修正

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,42 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "./layouts/Layout";
 import Question from "./components/QuestionPage/Question";
-import AnswerCard from "./layouts/AnswerCard";
-import AnswerCardContainer from "./parts/AnswerCardContainer";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
-import { getCheckedAnswerStringSelector } from "../redux/QuestionList/answerSelector";
-import { getAnswerListSelector } from "../redux/QuestionList/selector";
+import Answer from "./components/QuestionPage/Answer";
 
 const QuestionPage = () => {
-  const selector = useSelector((state: RootState) => state);
-  const checkedAnswerString = getCheckedAnswerStringSelector(selector);
-  const answerList = getAnswerListSelector(selector);
-  const [correctAnswerString, setCorrectAnswerString] = useState("");
-
-  // 問題の中から正解の答えを抽出
-  useEffect(() => {
-    const correctAnswerString = answerList.filter(
-      (list) => list.check === true
-    )[0];
-    // setCorrectAnswerString(correctAnswerString);
-    console.log(answerList)
-  }, []);
-
   return (
     <Layout>
       <Question />
-      <div className={`${checkedAnswerString ? "block" : "hidden"}`}>
-        {correctAnswerString === checkedAnswerString ? (
-          <AnswerCard>
-            <AnswerCardContainer answerResult="正解" color={"bg-green-200"} />
-          </AnswerCard>
-        ) : (
-          <AnswerCard>
-            <AnswerCardContainer answerResult="不正解" color={"bg-red-200"} />
-          </AnswerCard>
-        )}
-      </div>
+      <Answer />
     </Layout>
   );
 };
---
+++ src/pages/components/QuestionPage/Answer.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getCheckedAnswerStringSelector } from "../../../redux/QuestionList/answerSelector";
+import { getAnswerListSelector } from "../../../redux/QuestionList/selector";
+import { RootState } from "../../../redux/store";
+import AnswerCard from "../../layouts/AnswerCard";
+import AnswerCardContainer from "../../parts/AnswerCardContainer";
+
+const Answer = () => {
+  const selector = useSelector((state: RootState) => state);
+  const checkedAnswerString = getCheckedAnswerStringSelector(selector);
+  const answerList = getAnswerListSelector(selector);
+  const [correctAnswerString, setCorrectAnswerString] = useState("");
+
+  // 問題の中から正解の答えを抽出
+  useEffect(() => {
+    const correctAnswerString = answerList.filter(
+      (list) => list.check === true
+    )[0].body;
+    setCorrectAnswerString(correctAnswerString);
+    console.log(answerList);
+  }, [checkedAnswerString]);
+  return (
+    <div className={`${checkedAnswerString ? "block" : "hidden"}`}>
+      {correctAnswerString === checkedAnswerString ? (
+        <AnswerCard>
+          <AnswerCardContainer answerResult="正解" color={"bg-green-200"} />
+        </AnswerCard>
+      ) : (
+        <AnswerCard>
+          <AnswerCardContainer answerResult="不正解" color={"bg-red-200"} />
+        </AnswerCard>
+      )}
+    </div>
+  );
+};
+
+export default Answer;
