7d427132bc2b53640e6f20a497244b6237dfc17a
kazukazu2027 <kazu2000027@gmail.com>
2021-10-18 13:16:45 +0900

    ファイル整理

--- src/redux/storeAction/favoriteAction.ts
+++
@@ -1,9 +0,0 @@
-export const UPDATE_FAVORITE_WORD = "add favorite word";
-export const updateFavoriteWordAction = (checkedQuestionIds: string[]) => {
-  return {
-    type: UPDATE_FAVORITE_WORD as typeof UPDATE_FAVORITE_WORD,
-    checkedQuestionIds,
-  };
-};
-
-export type UpdateFavoriteType = ReturnType<typeof updateFavoriteWordAction>;
